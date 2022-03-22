ca8813f8e4e63189a710fd8a4ecbb0692e52bb68
HarshKakran <harshkakran05@gmail.com>
2022-03-23 01:53:04 +0530

    Added __str__ method to models

--- neer_sih/neer_bot/models.py
+++ neer_sih/neer_bot/models.py
@@ -13,13 +13,20 @@ class Bot(models.Model):
 
     name = models.CharField(max_length=200)
     status = models.CharField(max_length=64, null=True, blank=True, choices=BOT_STATUS_CHOICES)
+    battery_status = models.DecimalField(max_digits=3, decimal_places=0, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Trash(models.Model):
     bot = models.ForeignKey('Bot', on_delete=models.CASCADE)
-    date = models.DateField()
-    inorganic_trash = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    organic_trash = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    date_stamp = models.DateField(auto_add_now=True)
+    degradable_trash = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    non_degradable_trash = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.bot} - Degradable Trash: {self.degradable_trash} kg, Non-Degradable Trash: {self.non_degradable_trash} kg - {self.date_stamp}'
 
 
 class LastLocation(models.Model):
@@ -27,20 +34,42 @@ class LastLocation(models.Model):
     lat = models.DecimalField(max_digits=22, decimal_places=16, null=True, blank=True)
     lon = models.DecimalField(max_digits=22, decimal_places=16, null=True, blank=True)
     image = models.ImageField(upload_to='geo-locations/', null=True, blank=True)
-    time = models.DateTimeField()
+    time_stamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.bot} - ({self.lat}, {self.lon}) at {self.time}'
 
 
 class Obstacles(models.Model):
+    OBSTACLE_STATUS_REMOVED = 'removed'
+    OBSTACLE_STATUS_FOUND = 'found'
+
+    OBSTACLE_STATUS_CHOICES = [
+        (i, i) for i in [OBSTACLE_STATUS_REMOVED, OBSTACLE_STATUS_FOUND]
+    ]
+
     lat = models.DecimalField(max_digits=22, decimal_places=16)
     lon = models.DecimalField(max_digits=22, decimal_places=16)
+    status = models.CharField(max_length=64, choices=OBSTACLE_STATUS_CHOICES, null=True, blank=True)
+
+    def __str__(self):
+        return f'({self.lat}, {self.lon}) - {self.status}'
+
 
 class LastCollection(models.Model):
-    datetime = models.DateTimeField(auto_now_add=True)
-    location = models.ForeignKey('GarbageCollectionCenter', on_delete=models.SET_NULL, null=True)
+    date_time_stamp = models.DateTimeField(auto_now_add=True)
+    dump_centre = models.ForeignKey('GarbageCollectionCenter', on_delete=models.SET_NULL, null=True)
     weight = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.date_time_stamp}-{self.dump_centre}-{self.weight} kg'
+
 
 class GarbageCollectionCenter(models.Model):
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=128)
     state = models.CharField(max_length=128)
     pincode = models.DecimalField(max_digits=6, decimal_places=0)
+
+    def __str__(self):
+        return f'{self.address}, {self.city}, {self.state} - {self.pincode}'
