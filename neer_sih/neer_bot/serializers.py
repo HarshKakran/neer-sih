cd8ad3750418abef827a5df7cedc2ee03648b757
HarshKakran <harshkakran05@gmail.com>
2022-03-23 01:58:01 +0530

    Created serializers for models

---
+++ neer_sih/neer_bot/serializers.py
@@ -0,0 +1,47 @@
+from rest_framework import serializers
+from .models import LastLocation, Bot, Trash, Obstacles, LastCollection, GarbageCollectionCenter
+
+
+class BotSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bot
+        fields = ('name', 'status', 'battery')
+
+
+class LastLocationSerializer(serializers.ModelSerializer):
+    bot = serializers.SlugRelatedSerializer(slug_field='name', queryset=Bot.objects.all())
+
+    class Meta:
+        model = LastLocation
+        fields = ('bot', 'lat', 'lon', 'image', 'time_stamp')
+
+
+class TrashSerializer(serializers.ModelSerializer):
+    bot = serializers.StringRelatedField(slug_field='name', queryset=Bot.objects.all())
+
+    class Meta:
+        model = Trash
+        fields = ('bot', 'degradable_trash', 'non_degradable_trash', 'date_stamp')
+
+
+class ObstaclesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Obstacles
+        fields = ('lat', 'lon', 'status')
+
+
+class LastCollectionSerializer(serializers.ModelSerializer):
+    dump_centre = serializers.SerializerMethodField()
+
+    class Meta:
+        model = LastCollection
+        fields = ('date_time_stamp', 'dump_centre', 'weight')
+
+    def get_dump_centre(self, obj: LastCollection):
+        return obj.dump_centre
+
+
+class GarbageCollectionCenterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GarbageCollectionCenter
+        fields = ('address', 'city', 'state', 'pincode')
