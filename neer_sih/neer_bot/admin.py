5e9b9a011b3da8b9152155cce2d067bd06968b3b
HarshKakran <harshkakran05@gmail.com>
2022-03-23 01:57:48 +0530

    Added models to Django admin

--- neer_sih/neer_bot/admin.py
+++ neer_sih/neer_bot/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Bot, LastLocation, Trash
+from .models import Bot, Trash, LastLocation, Obstacles, LastCollection, GarbageCollectionCenter
 
 # Register your models here.
 admin.site.register(Bot)
 admin.site.register(LastLocation)
 admin.site.register(Trash)
+admin.site.register(Obstacles)
+admin.site.register(LastCollection)
+admin.site.register(GarbageCollectionCenter)
