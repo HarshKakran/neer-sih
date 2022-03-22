7c2561fb55c00d6404930da7ef02f04f18112d22
HarshKakran <harshkakran05@gmail.com>
2022-03-23 01:14:08 +0530

    Created LastCollection, GarbageCollectionCenter models, added status field to Bot model

--- neer_sih/neer_bot/models.py
+++ neer_sih/neer_bot/models.py
@@ -2,8 +2,17 @@ from django.db import models
 
 
 class Bot(models.Model):
+    BOT_STATUS_STUCK = "stuck"
+    BOT_STATUS_WORKING = "working"
+    BOT_STATUS_RETURNING = "returning"
+    BOT_STATUS_INACTIVE = "inactive"
+
+    BOT_STATUS_CHOICES = [
+        (i, i) for i in [BOT_STATUS_STUCK, BOT_STATUS_WORKING, BOT_STATUS_RETURNING, BOT_STATUS_INACTIVE]
+    ]
+
     name = models.CharField(max_length=200)
-    # lastLocation = models.ForeignKey('LastLocation', on_delete=models.)
+    status = models.CharField(max_length=64, null=True, blank=True, choices=BOT_STATUS_CHOICES)
 
 
 class Trash(models.Model):
@@ -24,3 +33,14 @@ class LastLocation(models.Model):
 class Obstacles(models.Model):
     lat = models.DecimalField(max_digits=22, decimal_places=16)
     lon = models.DecimalField(max_digits=22, decimal_places=16)
+
+class LastCollection(models.Model):
+    datetime = models.DateTimeField(auto_now_add=True)
+    location = models.ForeignKey('GarbageCollectionCenter', on_delete=models.SET_NULL, null=True)
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+
+class GarbageCollectionCenter(models.Model):
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=128)
+    state = models.CharField(max_length=128)
+    pincode = models.DecimalField(max_digits=6, decimal_places=0)
