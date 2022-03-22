a071197dccf339c519154297385b194f867f370a
HarshKakran <harshkakran05@gmail.com>
2022-03-23 05:06:03 +0530

    Added endpoint for TrashCollectionCentre post APIs

--- neer_sih/neer_bot/urls.py
+++ neer_sih/neer_bot/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from .views import BotStatusAPIView, BotListAPIView, TrashAPIView, LastLocationAPIView \
-    , ObstacleAPIView, ObstacleRetrieveUpdateAPIView, LastDumpAPIView
+    , ObstacleAPIView, ObstacleRetrieveUpdateAPIView, LastDumpAPIView, TrashCollectionCentreAPIView
 
 urlpatterns = [
     path('bot/', BotListAPIView.as_view(),name='bot_list'),
@@ -10,4 +10,5 @@ urlpatterns = [
     path('obstacle/', ObstacleAPIView.as_view(), name='obstacle'),
     path('obstacle/<str:slug>/', ObstacleRetrieveUpdateAPIView.as_view(), name='obstacle_retrieve_update'),
     path('last_dump/', LastDumpAPIView.as_view(), name='last_dump api'),
+    path('trash_collection_centre/', TrashCollectionCentreAPIView.as_view(), name='trash_collection_centre api'),
 ]
